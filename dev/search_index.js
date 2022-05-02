61374e1adb0dd52481a792248f4a84ab550db4a1
Documenter.jl <documenter@juliadocs.github.io>
2022-05-02 23:16:52 +0000

    build based on a0b6cb3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ClinicallyInformedDimensionalityReduction","category":"page"},{"location":"#ClinicallyInformedDimensionalityReduction","page":"Home","title":"ClinicallyInformedDimensionalityReduction","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ClinicallyInformedDimensionalityReduction.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ClinicallyInformedDimensionalityReduction]","category":"page"}]
+}
